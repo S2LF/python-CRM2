bcfb90da8cf7d08473c3e3d10c55ff1df83b1386
S2LF <sdeuhelf@gmail.com>
2022-05-11 23:18:06 +0200

    first commit

---
+++ CRM.py
@@ -0,0 +1,48 @@
+import re
+import string
+
+class User:
+    def __init__(self, first_name: str, last_name: str, phone_number: str="", address: str=""):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.address = address
+
+    def __repr__(self) -> str:
+        return f"User('{self.first_name}', '{self.last_name}')"
+
+    def __str__(self) -> str:
+        return f"{self.full_name}\n{self.phone_number}\n{self.address}"
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def _check_phone_number(self):
+        phone_number = re.sub(r"[+()\s]*", '', self.phone_number)
+        if len(phone_number) < 10 and phone_number.isdigit():
+            raise ValueError(f"Phone number {self.phone_number} is not valid")
+
+    def _check_names(self):
+        if not (self.first_name and self.last_name):
+            raise ValueError(f"firstname & lastname can't be empty")
+
+        special_characters = string.punctuation + string.digits
+        if any(char in special_characters for char in (self.first_name+self.last_name)):
+            raise ValueError(f"{self.full_name} is not valid")
+
+    def _checks(self):
+        self._check_names()
+        self._check_phone_number()
+
+if __name__ == "__main__":
+    from faker import Faker
+    fake = Faker(locale='fr_FR')
+    for _ in range(25):
+        # user = User(fake.first_name(), fake.last_name(), fake.phone_number(), fake.address())
+        user = User(fake.first_name(), fake.last_name(), fake.phone_number(), fake.address())
+        user._checks()
+        print(user)
+        # print(repr(user))
+        print("-" * 10)
+        
---
+++ README.md
@@ -0,0 +1,7 @@
+# python-CRM
+
+Before commit, add dependencies to `requirements.txt`
+
+```python
+pip freeze > requirements.txt
+```
